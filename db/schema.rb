4312a931f6e69fd68ff6478ebe01e5446834ceef
Kathleen French <kfrench09@gmail.com>
2016-12-30 18:50:10 -0500

    migration issues

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161230231223) do
+ActiveRecord::Schema.define(version: 20161230230112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,10 +22,9 @@ ActiveRecord::Schema.define(version: 20161230231223) do
     t.string   "address"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
+    t.float    "latitude"
+    t.float    "longitude"
   end
-
-  add_index "places", ["user_id"], name: "index_places_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
