705f0eb83c24f3b723fafbc17c20b05de75fed9e
Kathleen French <kfrench09@gmail.com>
2016-12-30 18:21:21 -0500

    ugh

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,7 +3,7 @@ class Place < ActiveRecord::Base
   geocoded_by :address
   after_validation :geocode
 
-  validates :name, presence: true
+  validates :name, :length=>{minimum: 3}, presence: true
   validates :address, presence: true
   validates :description, presence: true
 
--- db/migrate/20161230045536_alter_places_add_user_id_column.rb
+++ db/migrate/20161230045536_alter_places_add_user_id_column.rb
@@ -1,6 +1,4 @@
 class AlterPlacesAddUserIdColumn < ActiveRecord::Migration
   def change
-    add_column :places, :latitude, :float
-    add_index :places, :longitude, :float
   end
 end
